9d0eae04f51978934f4eaffed4ed36aa3535f3b0
Izabella <iza.cardoso129@gmail.com>
2022-03-06 17:00:56 -0300

    exemplos tipos primitivos js

---
+++ main.js
@@ -0,0 +1,18 @@
+const primeiroNumero = 3;
+const segundoNumero = 4;
+
+const operacaoMath = primeiroNumero + segundoNumero;
+
+console.log(operacaoMath);
+
+const cidade = 'são paulo'
+const input = 'São Paulo'
+
+const inputMaiusculo = input.toLowerCase();
+
+console.log(inputMaiusculo)
+
+const textoUm = 'Alura';
+const textoDois = 'Alura';
+
+console.log(textoUm===textoDois);
