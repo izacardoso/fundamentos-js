ade683e930fb90f7d8fa8f04eb3dd6b1b9da22ad
Izabella <iza.cardoso129@gmail.com>
2022-03-06 17:43:43 -0300

    tipos de variaveis

---
+++ script.js
@@ -0,0 +1,31 @@
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+
+// console.log(area);
+
+// let forma = "retângulo"
+// let altura = 5;
+// let comprimento = 7;
+
+// if (forma === "triangulo"){
+//     area = altura * comprimento
+// } else {
+//     area = (altura * comprimento) /2
+// }
+
+// console.log(area)
+
+const forma = "retângulo"
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma==="triangulo"){
+    area = altura * comprimento
+} else{
+    area = (altura * comprimento) /2
+}
+
+console.log(area)
